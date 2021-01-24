9b4fcae3ab12b0900120a1ded4e70484bc0e9893
rajeka <rekanayake@yahoo.com>
2021-01-24 15:11:43 -0800

    flipped the url code

--- server.js
+++ server.js
@@ -3,18 +3,21 @@ var express = require('express');
 //creat our app
 var app = express();
 const PORT = process.env.PORT || 3000;
-//folder to serve
-app.use(express.static('public'));
+
 // turn traffic from https -> https
 app.use(function (req, res, next){
-  if (req.headers['x-forwarded-proto'] === 'http') {
-    next();
-  } else {
-    // redirect if not http
+  if (req.headers['x-forwarded-proto'] === 'https') {
     console.log('req.hostname: '+ req.hostname + 'req.url: ' + req.url);
     res.redirect('http://' + req.hostname + req.url);
+
+  } else {
+    // redirect if not http
+    next();
   }
 });
+
+//folder to serve
+app.use(express.static('public'));
 
 //start the server
 app.listen(PORT, function () {
